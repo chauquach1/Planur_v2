d5f5cc8773d4863efeec1e14ffb9aacef959cf47
chauquach1 <chau268@gmail.com>
2023-11-05 15:43:54 -0800

    Revert "fixed logoutbtn"
    This reverts commit 7ee04d1184e8615b67dc168bcf8dad877617ae7a.

--- src/app/components/auth-components/_log-out/LogOutBtn.jsx
+++ src/app/components/auth-components/_log-out/LogOutBtn.jsx
@@ -1,11 +1,16 @@
+"use client";
 import LogOutFunction from "./log-out-csr";
 
 export default async function LogOutBtn() {
+  const handleSignOut = async () => {
+    LogOutFunction();
+  };
+
   return (
     <>
       <button
         className="bg-blue-500 hover:bg-zinc-600 text-white font-bold py-2 px-4 rounded"
-        onClick={LogOutFunction}
+        onClick={handleSignOut}
       >
         Logout
       </button>
