d32aa43e577060a0085c2032be8fffadd08c3c8d
chauquach1 <chau268@gmail.com>
2023-11-14 21:59:34 -0800

    merge conflict in user/route.js

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,72 +1,37 @@
-// import connectMongoDB from "../../libs/mongo/mongodb";
-// import User from "../../models/user";
-// import { NextResponse } from "next/server";
+import connectMongoDB from "../../libs/mongo/mongodb";
+import { NextResponse } from "next/server";
 
-// export default async function userRouter(request) {
-//   try {
-//     await connectMongoDB();
+export async function GET(request) {
+  try {
+    const client = await connectMongoDB();
+    const db = client.db('planur_v2');
+    const collection = db.collection("users");
 
-//     // Handle GET User request
-//     if (request.method === "GET") {
-//       const { uuid } = request.query;
+    // Get 'uuid' from request.query
+    const { uuid } = await request.query;
+    console.log('UUID:', uuid);
 
-//       if (!uuid) {
-//         return NextResponse.error("UUID parameter is missing", { status: 400 });
-//       }
+    // Check if uuid is provided
+    if (!uuid) {
+      return NextResponse.json({ error: "UUID parameter is missing" }, { status: 400 });
+    }
 
-//       const user = await User.findOne({ uuid });
+    // Find the user in the database
+    const user = await collection.findOne({ uuid: uuid });
+    if (!user) {
+      console.log('User not found');
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
 
-//       if (!user) {
-//         return NextResponse.error("User not found", { status: 404 });
-//       }
-
-//       return NextResponse.json(user, { status: 200 });
-//     } 
-
-//     // Handle POST User request
-//     else if (request.method === "POST") {
-//       const { firstname, lastname, email, password, uuid } =
-//         await request.json();
-
-//       const newUser = new User({ firstname, lastname, email, password, uuid });
-
-//       await newUser.save(); // Create a new user in MongoDB
-
-//       return NextResponse.json({ message: "User created." }, { status: 201 });
-      
-//     }
-
-//     // Handle PUT User request
-//     else if (request.method === "PUT") {
-//       const { uuid, firstname, lastname, email, password } =
-//         await request.json();
-
-//       const updatedUser = await User.findOneAndUpdate(
-//         { uuid },
-//         { firstname, lastname, email, password },
-//         { new: true }
-//       ); // Update user data in MongoDB
-      
-//       return NextResponse.json(updatedUser, { status: 200 });
-
-//     }
-
-//     // Handle DELETE User request
-//     else if (request.method === "DELETE") {
-//       const { uuid } = await request.json();
-
-//       await User.findOneAndDelete({ uuid }); // Delete user from MongoDB
-
-//       return NextResponse.json({ message: "User deleted." }, { status: 200 });
-
-//     }
-
-//     // Unsupported HTTP method
-//     else {
-//       return NextResponse.error("Unsupported method", { status: 405 });
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     return NextResponse.error("Server error", { status: 500 });
-//   }
-// }
+    // Return the found user
+    return NextResponse.json({ user }, { status: 200 });
+  } catch (error) {
+    console.log("Error fetching user data:", error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  } finally {
+    // Close the client if it's defined
+    if (client) {
+      await client.close();
+    }
+  }
+}
