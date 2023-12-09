6dfaed0629f950c424b9ccabaf4afb5796853ba2
chauquach1 <chau268@gmail.com>
2023-12-08 17:31:43 -0800

    created sample stops for testing

---
+++ src/app/_tests_/sampleStops.js
@@ -0,0 +1,71 @@
+let sampleStops = [
+  {
+    "_id": "65619185f0a35c213aeda7df",
+    "stopName": "Shibuya",
+    "stopAddress": {
+      "street": "717 Merit Dr",
+      "city": "San Marcos",
+      "state": "California",
+      "zip": "92078",
+      "country": "United States"
+    },
+    "stopArrival": "2024-01-03T00:00:00Z",
+    "stopDeparture": "2024-01-04T00:00:00Z",
+    "stopType": "Landmark",
+    "stopTransportation": "Car",
+    "stopInterest": "Must-Go",
+    "stopResNum": "",
+    "stopNotes": "",
+    "stopPhoneNumber": "8589223709",
+    "stopEmail": "chau268@gmail.com",
+    "createdAt": "2023-11-25T06:17:41.832Z",
+    "updatedAt": "2023-11-25T06:17:41.832Z",
+    "__v": 0
+  },
+  {
+    "_id": "656194a0637a2dfcc1a182d3",
+    "stopName": "Some Place",
+    "stopAddress": {
+      "street": "717 Merit Dr",
+      "city": "San Marcos",
+      "state": "California",
+      "zip": "92078",
+      "country": "United States"
+    },
+    "stopArrival": "2024-01-02T00:00:00Z",
+    "stopDeparture": "2024-01-05T00:00:00Z",
+    "stopType": "Landmark",
+    "stopTransportation": "Car",
+    "stopInterest": "High",
+    "stopResNum": "",
+    "stopNotes": "",
+    "stopPhoneNumber": "8589223709",
+    "stopEmail": "chau268@gmail.com",
+    "createdAt": "2023-11-25T06:30:56.927Z",
+    "updatedAt": "2023-11-25T06:30:56.927Z",
+    "__v": 0
+  },
+  {
+    "_id": "6562a32efee4f47764f27099",
+    "stopName": "tgestsets",
+    "stopAddress": {
+      "street": "",
+      "city": "",
+      "state": "",
+      "zip": "",
+      "country": ""
+    },
+    "stopArrival": "2023-12-09T00:00:00Z",
+    "stopDeparture": "2023-12-28T00:00:00Z",
+    "stopType": "Restaurant",
+    "stopTransportation": "",
+    "stopInterest": "Must-Go",
+    "stopResNum": "",
+    "stopNotes": "",
+    "stopPhoneNumber": "",
+    "stopEmail": "",
+    "createdAt": "2023-11-26T01:45:18.829Z",
+    "updatedAt": "2023-11-26T01:45:18.829Z",
+    "__v": 0
+  }
+]
