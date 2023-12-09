2468626ad3c468f7c7411e65169ab5a57fd5f3c9
chauquach1 <chau268@gmail.com>
2023-12-08 17:34:16 -0800

    created samplePacklist for testing

---
+++ src/app/_tests_/samplePacklist.js
@@ -0,0 +1,52 @@
+let samplePacklist = {
+  _id: "65629dbdfee4f47764f27092",
+  clothes: {
+    shirts: true,
+    pants: false,
+    shorts: false,
+    sweater: false,
+    underwear: false,
+  },
+  luggage: {
+    backpack: false,
+    carryon: true,
+    dufflebag: false,
+    suitcase: false,
+    garmentbag: false,
+  },
+  toiletries: {
+    toothbrush: true,
+    toothpaste: false,
+    shampoo: false,
+    conditioner: false,
+    sunscreen: false,
+  },
+  miscellaneous: {
+    cellphone: false,
+    laptop: false,
+    tablet: false,
+    passport: true,
+    medication: false,
+  },
+  emergencyContact: {
+    firstName: "Test",
+    lastName: "TEst",
+    relationship: "test",
+    phoneNumber: "777-777-7777",
+    email: "test@gmail.com",
+    address: {
+      street: "180 Sabin Way",
+      city: "Lochbuie",
+      state: "CO",
+      zip: "80603",
+      country: "USA",
+    },
+  },
+  createdAt: {
+    $date: "2023-11-26T01:22:06.002Z",
+  },
+  updatedAt: {
+    $date: "2023-11-26T01:22:06.002Z",
+  },
+  __v: 0,
+};
