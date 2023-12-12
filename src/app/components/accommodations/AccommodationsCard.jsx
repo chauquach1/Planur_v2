5e88bf41cc6d7d051220215f3593d17cd33bec18
chauquach1 <chau268@gmail.com>
2023-12-12 14:59:47 -0800

    updated AccomsCard

--- src/app/components/accommodations/AccommodationsCard.jsx
+++ src/app/components/accommodations/AccommodationsCard.jsx
@@ -1,5 +1,5 @@
 import { Card, CardBody } from "@nextui-org/react";
-// import { format } from "date-fns";
+import normalDateFormat from "../../_utils/normalDateFormat";
 
 export default function AccommodationsCard({
   accom,
@@ -7,11 +7,14 @@ export default function AccommodationsCard({
 }) {
   // const checkInDate = format(new Date(accom.accomCheckIn), "PP");
   // const checkOutDate = format(new Date(accom.accomCheckOut), "PP");
+  let address = accom.accomAddress;
+  const checkInDate = normalDateFormat(accom.accomCheckIn);
+  const checkOutDate = normalDateFormat(accom.accomCheckOut);
 
   return (
     <Card
       isHoverable
-      isPressable
+      // isPressable
       // isBlurred
       className=" w-full border my-1 shadow-lg bg-white"
       // onPress={() =>
@@ -21,16 +24,37 @@ export default function AccommodationsCard({
       <CardBody>
         <div className="flex flex-col">
           <div className="row flex flex-row flex-wrap justify-between items-baseline">
-            <p className="text-md inline-block align-baseline">
+            <p className="text-lg inline-block align-baseline">
               {accom.accomName}
             </p>
           </div>
           <p className="text-small text-default-500">
-            {/* {checkInDate} - {checkOutDate} */}
+            {checkInDate} - {checkOutDate}
           </p>
           <p className="text-small text-default-500 align-baseline">
             {accom.accomType}
           </p>
+          <div
+            id="accom-contact-info"
+            className="flex flex-row flex-wrap font-light gap-2"
+          >
+            <h1 className="font-normal">Email:</h1>
+            <h1 className="">{address.email || "-"}</h1>
+            <h1 className="font-normal">Phone Number:</h1>
+            <h1 className="">{address.accomPhoneNumber  || "-"}</h1>
+          </div>
+          <h1 className="font-normal">Confirmation: <span className="font-light">{accom.accomResNum}</span></h1>
+          <div
+            id="accom-address"
+            className="flex flex-row flex-wrap font-light gap-2"
+          >
+            <h1 className="font-normal">Address:</h1>
+            <h1 className="col-span-3">{address.street}</h1>
+            <h1 className="col-span-3">{address.city},</h1>
+            <h1 className="col-span-2">{address.state}</h1>
+            <h1 className="col-span-2">{address.zip},</h1>
+            <h1 className="col-span-2">{address.country}</h1>
+          </div>
         </div>
       </CardBody>
     </Card>
--- src/app/components/accommodations/AccomsSection.jsx
+++ src/app/components/accommodations/AccomsSection.jsx
@@ -3,22 +3,9 @@ import AccommodationsCard from "./AccommodationsCard";
 export default function AccomsSection({ accoms }) {
   let accom = accoms[0];
   console.log("accom: ", accom);
-  let address = accom.accomAddress;
   return (
     <SectionContainer category="Accommodations">
       <AccommodationsCard accom={accom} />
-      {/* <h1>{accom.accomName}</h1>
-      <h1>{accom.accomCheckIn}</h1>
-      <h1>{accom.accomCheckOut}</h1>
-      <h1>{accom.accomType}</h1>
-      <h1>{address.email}</h1>
-      <h1>{accom.accomResNum}</h1>
-      <h1>{address.accomPhoneNumber}</h1>
-      <h1>{address.accomAddress}</h1>
-      <h1>{address.accomCity}</h1>
-      <h1>{address.accomState}</h1>
-      <h1>{address.accomZip}</h1>
-      <h1>{address.accomCountry}</h1> */}
     </SectionContainer>
   );
 }
