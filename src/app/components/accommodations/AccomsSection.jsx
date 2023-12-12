d1b84e5c0b9cecd1b82ec9bb13cf94997b9378e9
chauquach1 <chau268@gmail.com>
2023-12-12 15:02:01 -0800

    AccomsSection renders AccomsCard for each accom in accom array

--- src/app/components/accommodations/AccomsSection.jsx
+++ src/app/components/accommodations/AccomsSection.jsx
@@ -1,11 +1,13 @@
 import SectionContainer from "../trip-components/SectionContainer";
 import AccommodationsCard from "./AccommodationsCard";
 export default function AccomsSection({ accoms }) {
-  let accom = accoms[0];
-  console.log("accom: ", accom);
+  // let accom = accoms[0];
+  // console.log("accom: ", accom);
   return (
     <SectionContainer category="Accommodations">
-      <AccommodationsCard accom={accom} />
+      {accoms.map((accom) => {
+        return <AccommodationsCard accom={accom} />;
+      })}
     </SectionContainer>
   );
 }
