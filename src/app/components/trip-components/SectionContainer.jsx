fdee1562c948cf3804205cd3090c5dee99aa6d4c
chauquach1 <chau268@gmail.com>
2023-12-13 23:45:43 -0800

    sections are collapsible via button click

--- src/app/components/trip-components/SectionContainer.jsx
+++ src/app/components/trip-components/SectionContainer.jsx
@@ -1,8 +1,22 @@
+import { set } from "date-fns";
+import { useState } from "react";
 export default function SectionContainer({children, category}) {
+  const [showCategory, setShowCategory] = useState(true);
+  const [btnText, setBtnText] = useState(true);
+  const buttonClicked = () => {
+    setBtnText(!btnText);
+    setShowCategory(!showCategory);
+  }
   return (
     <>
-      <h1 className="font-bold">{category}</h1>
-      <div id={`${category}-section`} className="flex flex-col gap-1 xl:px-4 2xl:px-6">
+      <div className="flex flex-row w-full justify-between border-y-2 py-4 pe-10">
+        <h1 className="font-bold text-2xl">{category}</h1>
+        <button onClick={() => buttonClicked()} className="">{btnText ? `Collapse ${category}` :`Show ${category}`}</button>
+      </div>
+      <div
+        id={`${category}-section`}
+        className={`${showCategory ? 'flex' : 'hidden'} flex flex-col gap-1 xl:px-4 2xl:px-6`}
+      >
         {children}
       </div>
     </>
