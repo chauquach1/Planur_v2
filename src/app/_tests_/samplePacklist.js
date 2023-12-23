d47873bd531adfe00e8cf3532271b34c8cb29544
chauquach1 <chau268@gmail.com>
2023-12-22 18:32:01 -0800

    updated packList data and CategoryBtn:

--- src/app/_tests_/samplePacklist.js
+++ src/app/_tests_/samplePacklist.js
@@ -1,53 +1,8 @@
 let samplePacklist = {
   _id: "65629dbdfee4f47764f27092",
-  clothes: {
-    shirts: true,
-    pants: false,
-    shorts: false,
-    sweater: false,
-    underwear: false,
-    jacket: true, // new key
-    hat: false, // new key
-    socks: true, // new key
-    gloves: false, // new key
-    scarf: true, // new key
-  },
-  luggage: {
-    backpack: false,
-    carryon: true,
-    dufflebag: false,
-    suitcase: false,
-    garmentbag: false,
-    totebag: true, // new key
-    briefcase: false, // new key
-    travelPillow: true, // new key
-    luggageLock: false, // new key
-    luggageTag: true, // new key
-  },
-  toiletries: {
-    toothbrush: true,
-    toothpaste: false,
-    shampoo: false,
-    conditioner: false,
-    sunscreen: false,
-    deodorant: true, // new key
-    bodyWash: false, // new key
-    razor: true, // new key
-    moisturizer: false, // new key
-    hairBrush: true, // new key
-  },
-  miscellaneous: {
-    cellphone: false,
-    laptop: false,
-    tablet: false,
-    passport: true,
-    medication: false,
-    charger: true, // new key
-    headphones: false, // new key
-    travelAdapter: true, // new key
-    sunglasses: false, // new key
-    notebook: true, // new key
-  },
+  clothes: ["shirts", "jacket", "socks", "scarf"],
+  toiletries: ["toothbrush", "deodorant", "razor", "hairBrush"],
+  miscellaneous: ["passport", "charger", "travelAdapter", "notebook"],
   createdAt: {
     $date: "2023-11-26T01:22:06.002Z",
   },
--- src/app/libs/packListItems.js
+++ src/app/libs/packListItems.js
@@ -69,7 +69,7 @@ const packListItems = {
     "Tylenol",
     "Wipes",
   ],
-  BackpackCarryOn: [
+  Backpack: [
     "Blanket",
     "Book",
     "Camera",
