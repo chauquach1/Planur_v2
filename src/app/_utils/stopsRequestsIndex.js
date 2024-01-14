2a88904d5c137515d0bd549fd9a170020b68bb6c
chauquach1 <chau268@gmail.com>
2024-01-13 17:50:39 -0800

    created stopsRequestsIndex file for stops HTTP requests

---
+++ src/app/_utils/stopsRequestsIndex.js
@@ -0,0 +1,44 @@
+// GET /api/stops?stopId=123
+export async function fetchStop (stopId) {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/stops?stopId=${stopId}`
+    );
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || "Something went wrong!");
+    }
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// PUT /api/stops
+export async function putStop (stop) {
+  const response = await fetch("http://localhost:3000/api/stops", {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(stop)
+  });
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+}
+
+// POST /api/stops
+export async function postStop (tripId, stop) {
+  const response = await fetch(`http://localhost:3000/api/stops?tripId=${tripId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(stop)
+  });
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+}
