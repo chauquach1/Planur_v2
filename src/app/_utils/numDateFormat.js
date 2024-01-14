235e443e739f0383a5b7014844570406c7236826
chauquach1 <chau268@gmail.com>
2024-01-13 17:46:11 -0800

    deleted unused date formatting files

--- src/app/_utils/numDateFormat.js
+++
@@ -1,7 +0,0 @@
-import { format, parseISO } from "date-fns";
-export default function numFormatDate(date) {
-  const parsedDate = parseISO(date);
-  const formattedDate = format(new Date(parsedDate), "yyyy-MM-dd");
-
-  return formattedDate;
-}
