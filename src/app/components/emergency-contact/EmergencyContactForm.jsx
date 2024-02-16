da1d3ae0e99bd2e62fb64d8471a7be80d432124a
chauquach1 <chau268@gmail.com>
2024-02-16 14:56:53 -0800

    updated onChange functions for inputs in EmergencyContactsForm component

--- src/app/components/emergency-contact/EmergencyContactForm.jsx
+++ src/app/components/emergency-contact/EmergencyContactForm.jsx
@@ -1,4 +1,4 @@
-import { Input } from "@nextui-org/react";
+import { Input, Button } from "@nextui-org/react";
 import { useState, useEffect } from "react";
 export default function EmergencyContactForm({emergencyContactsProps: {contactsIndex, setContactsIndex, activeContact, setActiveContact, showContactForm, setShowContactForm}, requestProps}) {
   const [initialState, setInitialState] = useState(activeContact || {});
@@ -101,107 +101,151 @@ export default function EmergencyContactForm({emergencyContactsProps: {contactsI
   const isVisible = showContactForm ? "fixed flex" : "hidden";
 
   return (
-    <div id="emergency-initialState-form" className="grid grid-cols-8 p-1 gap-1">
-      <Input
-        key="firstName"
-        label="First Name"
-        value={initialState.firstName || ""}
-        onChange={(e) => handleInputChange("firstName", e.target.value)}  
-        autoComplete="off"
-        variant="faded"
-        size="sm"
-        className="col-span-3"
-      />
-      <Input
-        key="lastName"
-        label="Last Name"
-        value={initialState.lastName || ""}
-        onChange={(e) => handleInputChange("lastName", e.target.value)}
-        autoComplete="off"
-        variant="faded"
-        size="sm"
-        className="col-span-3"
-      />
-      <Input
-        key="relationship"
-        label="Relationship"
-        value={initialState.relationship || ""}
-        onChange={(e) => handleInputChange("relationship", e.target.value)}
-        autoComplete="off"
-        variant="faded"
-        size="sm"
-        className="col-span-2"
-      />
-      <Input
-        key="phoneNumber"
-        label="Phone Number"
-        value={initialState.phoneNumber || ""}
-        onChange={(e) => handleInputChange("phoneNumber", e.target.value)}
-        autoComplete="off"
-        variant="faded"
-        size="sm"
-        className="col-span-4"
-      />
-      <Input
-        key="email"
-        label="Email"
-        value={initialState.email || ""}
-        onChange={(e) => handleInputChange("email", e.target.value)}
-        autoComplete="off"
-        variant="faded"
-        size="sm"
-        className="col-span-4"
-      />
-      <Input
-        key="street"
-        label="Street"
-        value={initialState.address ? initialState.address.street : ""}
-        onChange={(e) => handleNestedInputChange("address", "street", e.target.value)}
-        autoComplete="off"
-        variant="faded"
-        size="sm"
-        className="col-span-5"
-      />
-      <Input
-        key="city"
-        label="City"
-        value={initialState.address ? initialState.address.city : ""}
-        onChange={(e) => handleNestedInputChange("address", "city", e.target.value)}
-        autoComplete="off"
-        variant="faded"
-        size="sm"
-        className="col-span-3"
-      />
-      <Input
-        key="state"
-        label="State"
-        value={initialState.address ? initialState.address.state : ""}
-        onChange={(e) => handleNestedInputChange("address", "state", e.target.value)}
-        autoComplete="off"
-        variant="faded"
-        size="sm"
-        className="col-span-2"
-      />
-      <Input
-        key="zip"
-        label="Zip"
-        value={initialState.address ? initialState.address.zip : ""}
-        onChange={(e) => handleNestedInputChange("address", "zip", e.target.value)}
-        autoComplete="off"
-        variant="faded"
-        size="sm"
-        className="col-span-3"
-      />
-      <Input
-        key="country"
-        label="Country"
-        value={initialState.address ? initialState.address.country : ""}
-        onChange={(e) => handleNestedInputChange("address", "country", e.target.value)}
-        autoComplete="off"
-        variant="faded"
-        size="sm"
-        className="col-span-3"
-      />
+    <div
+      className={`${isVisible} right-0 top-0 mx-auto
+    flex-col h-full w-full md:max-w-[325px] lg:max-w-[400px] xl:max-w-[500px] 2xl:max-w-[600px] p-4 pb-2 bg-slate-300 rounded-tl-xl ms-2`}
+    >
+      <button
+        className="self-end text-red-500"
+        onClick={() => setShowContactForm(false)}
+      >
+        x Close
+      </button>
+
+      <form
+        id="emergency-initialState-form"
+        action={handleSubmit}
+        // className="grid grid-cols-8 gap-1 bg-white rounded-xl p-2"
+        className="flex flex-col gap-2 bg-white rounded-xl p-2"
+      >
+        <Input
+          key="firstName"
+          label="First Name"
+          value={initialState.firstName || ""}
+          onChange={(e) => handleInputChange("firstName", e.target.value)}
+          autoComplete="off"
+          variant="faded"
+          size="sm"
+          className="col-span-3"
+        />
+        <Input
+          key="lastName"
+          label="Last Name"
+          value={initialState.lastName || ""}
+          onChange={(e) => handleInputChange("lastName", e.target.value)}
+          autoComplete="off"
+          variant="faded"
+          size="sm"
+          className="col-span-3"
+        />
+        <Input
+          key="relationship"
+          label="Relationship"
+          value={initialState.relationship || ""}
+          onChange={(e) => handleInputChange("relationship", e.target.value)}
+          autoComplete="off"
+          variant="faded"
+          size="sm"
+          className="col-span-2"
+        />
+        <Input
+          key="phoneNumber"
+          label="Phone Number"
+          value={initialState.phoneNumber || ""}
+          onChange={(e) => handleInputChange("phoneNumber", e.target.value)}
+          autoComplete="off"
+          variant="faded"
+          size="sm"
+          className="col-span-4"
+        />
+        <Input
+          key="email"
+          label="Email"
+          value={initialState.email || ""}
+          onChange={(e) => handleInputChange("email", e.target.value)}
+          autoComplete="off"
+          variant="faded"
+          size="sm"
+          className="col-span-4"
+        />
+        <Input
+          key="street"
+          label="Street"
+          value={initialState.address ? initialState.address.street : ""}
+          onChange={(e) =>
+            handleNestedInputChange("address", "street", e.target.value)
+          }
+          autoComplete="off"
+          variant="faded"
+          size="sm"
+          className="col-span-5"
+        />
+        <Input
+          key="city"
+          label="City"
+          value={initialState.address ? initialState.address.city : ""}
+          onChange={(e) =>
+            handleNestedInputChange("address", "city", e.target.value)
+          }
+          autoComplete="off"
+          variant="faded"
+          size="sm"
+          className="col-span-3"
+        />
+        <Input
+          key="state"
+          label="State"
+          value={initialState.address ? initialState.address.state : ""}
+          onChange={(e) =>
+            handleNestedInputChange("address", "state", e.target.value)
+          }
+          autoComplete="off"
+          variant="faded"
+          size="sm"
+          className="col-span-2"
+        />
+        <Input
+          key="zip"
+          label="Zip"
+          value={initialState.address ? initialState.address.zip : ""}
+          onChange={(e) =>
+            handleNestedInputChange("address", "zip", e.target.value)
+          }
+          autoComplete="off"
+          variant="faded"
+          size="sm"
+          className="col-span-3"
+        />
+        <Input
+          key="country"
+          label="Country"
+          value={initialState.address ? initialState.address.country : ""}
+          onChange={(e) =>
+            handleNestedInputChange("address", "country", e.target.value)
+          }
+          autoComplete="off"
+          variant="faded"
+          size="sm"
+          className="col-span-3"
+        />
+        <div
+          id="submit-btn-container"
+          className="flex flex-row mt-2 w-full justify-center pt-2"
+        >
+          <Button
+            color="success"
+            radius="full"
+            className="text-white"
+            type="submit"
+            // disabled={isSubmitting}
+            size="sm"
+          >
+            {requestProps.requestType === "POST"
+              ? "Add Emergency Contact"
+              : "Update Emergency Contact"}
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
